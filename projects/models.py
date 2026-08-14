d8d18962015dc5a16eba6b066df2d4c4acf6ade5
Волков Егор Юрьевич <egor.volkov.2005.12.03@gmail.com>
2026-08-14 17:19:41 +0300

    Add project model and admin panel

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    short_description = models.TextField()
+    description = models.TextField()
+    stack = models.CharField(max_length=300)
+    github_url = models.URLField(blank=True)
+    image = models.ImageField(
+        upload_to='projects/',
+        blank=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.title
